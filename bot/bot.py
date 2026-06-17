3d66557ac082d121ddb93ca3998c7eb7559cd9c9
umbrellacorp111 <163636061+umbrellacorp111@users.noreply.github.com>
2026-06-17 20:26:20 +0400

    Add files via upload

--- bot/bot.py
+++ bot/bot.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from app.config import settings
